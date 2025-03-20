9c79eef4d3ee8e66ffc69c09624368537dbedfc6
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-03-20 22:30:37 +0900

    add: index signature

--- practice/README.object.md
+++ practice/README.object.md
@@ -1,3 +1,65 @@
 # オブジェクトの基本と型
 
 TypeScriptではただのオブジェクトが幅広く使用されている。クラスが使われないことも多い。
+
+## オブジェクトの型
+
+```typescript
+// オブジェクトの型
+const obj: {
+    name: string,
+    age: number
+} = {
+    name: 'take',
+    age: 38
+}
+
+// これはエラーになる
+// const obj2: {
+//     name: string,
+//     age: number
+// } = {
+//     name: 'take',
+//     age: true
+// }
+```
+
+## type文で型に別名をつける
+
+```typescript
+type FooBarObj = {
+    foo: number;
+    bar: string;
+};
+const objFooBar: FooBarObj = {
+    foo: 0,
+    bar: 'string'
+}
+
+type UserId = string;
+const id: UserId = "uhyo";
+```
+
+### type文の特徴
+1. type文はどんな型にも別名をつけることができる
+2. あくまで型に別名をつけるだけである(すでにある型に別名をつけるだけのものである)
+
+## interfaceで型を定義する
+
+```typescript
+interface FooBarObj2 {
+    foo: number;
+    bar: string;
+}
+
+const objFooBar2: FooBarObj2 = {
+    foo: 0,
+    bar: 'string'
+}
+```
+
+### interfaceの特徴
+1. オブジェクト型のみ扱える
+
+ほとんどの場合、interface宣言はtype文で代用できる。interface宣言は使用せず、type文のみを使用するという流儀もある。
+
--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,17 +1,105 @@
-import { createInterface } from "readline";
+// オブジェクトの型
+// const obj: {
+//     name: string,
+//     age: number
+// } = {
+//     name: 'take',
+//     age: 38
+// }
 
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// オブジェクトの型
+// これはエラーになる
+// const obj2: {
+//     name: string,
+//     age: number
+// } = {
+//     name: 'take',
+//     age: true
+// }
 
-const message = {
-    good: "0以上の数値が入力されました！",
-    bad: "負の値を入力しないでください！",
+// type文で型に別名をつける
+type FooBarObj = {
+    foo: number;
+    bar: string;
+};
+const objFooBar: FooBarObj = {
+    foo: 0,
+    bar: 'string'
 }
 
-rl.question("数値を入力してください：", (answer) => {
-    const num = Number(answer);
-    console.log(message[num >= 0 ? "good" : "bad"]);
-    rl.close();
-});
+type UserId = string;
+const id: UserId = "uhyo";
+
+
+// type文の特徴
+// 1. type文はどんな型にも別名をつけることができる
+// 2. あくまで型に別名をつけるだけである(すでにある型に別名をつけるだけのものである)
+
+// interfaceで型を定義する
+// interface宣言: 型名を新規登録する方法
+// interfaceの特徴
+// 1. オブジェクト型のみ扱える
+
+// ほとんどの場合、interface宣言はtype文で代用できる。
+// interface宣言は使用せず、type文のみを使用するという流儀もある。
+
+interface FooBarObj2 {
+    foo: number;
+    bar: string;
+}
+
+const objFooBar2: FooBarObj2 = {
+    foo: 0,
+    bar: 'string'
+}
+
+// 任意のプロパティ名を許容する型(インデックスシグネチャ)
+// 使用用途: プロパティ名が動的に決まる状況
+// [キー名: string]: 型と書くのが基本的な形(任意のstring型のキーに対してという意味)
+
+type PriceData = {
+    [key: string]: number;
+}
+
+const data: PriceData = {
+    apple: 100,
+    banana: 200,
+    orange: 300
+}
+
+data.chicken = 500; // 追加できる
+// data.beef = 'string'; // エラーになる
+data.弁当 = 1000; // 追加できる
+// data.弁当 = "foo"; // エラーになる
+
+// インデックスシグネチャは、TypeScriptが保証する型安全性を破壊するため使用する際、注意が必要
+type MyObj = {
+    [key: string]: number;
+};
+const obj3: MyObj = {
+    foo: 123,
+};
+
+const num: number = obj3.bar; // ここでエラーが発生しない
+console.log(num); // numberではなく、undefinedとなる
+// 存在しないプロパティにアクセスするものはそもそもコンパイルエラーとなるはずだが、それを可能にしてしまうのがインデックスシグネチャ
+// この特性により、型安全性が破壊されてしまう。
+
+// リテラル型やユニオン型
+
+// リテラル型
+const propName: 'foo' = 'foo';
+const obj = {
+    [propName]: 123,
+};
+
+console.log(obj.foo); // 123
+
+// // ユニオン型
+// type PropName = 'foo' | 'bar';
+// const propName2: PropName = 'foo';
+// const obj2 = {
+//     [propName2]: 456,
+// };
+
+// console.log(obj2.foo); // 456
