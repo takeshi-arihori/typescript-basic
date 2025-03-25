351f09e3d4e3306cc322781d7d4cf2ac3501ea17
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-03-25 22:03:05 +0900

    update: type arguments, generics

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,53 +1,29 @@
-// オプショナルなプロパティ
-// あってもなくてもよいプロパティ
+// 型引数
 
-interface User {
+// 型引数は type文(またはinterface文)で型を作成する時に宣言する
+// 「User<T>型はnameプロパティがstring型で、childプロパティがT型であるオブジェクトの型」ということになる
+
+// 似たような構造を持つ方を色々と使用したい場合に型引数を使用する
+type User<T> = {
     name: string;
-    age?: number;
-}
-
-const userA: User = {
-    name: 'Taro',
-    age: 25
+    child: T;
 };
 
-const userB: User = {
-    name: 'Jiro'
+const user: User<number> = {
+    name: "John",
+    child: 1000,
 };
 
-// 読み取り専用プロパティ
-// readonlyをつけるとプロパティが読み取り専用になる
-// プロパティを変更する必要がない場合はreadonlyを使うとよい
-interface User2 {
-    readonly name: string;
-    age: number;
-}
-
-const userC: User2 = {
-    name: 'Taro',
-    age: 25
+// 型引数を持つ型はジェネリック型と呼ばれる
+type Family<Parent, Child> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+};
+const obj: Family<number, boolean> = {
+    mother: 0,
+    father: 1,
+    child: true,
 };
 
-// userC.name = 'Jiro'; // エラー
 
-// typeofキーワードで変数の方を得る
-// const num: number = 0;
-// type T = typeof num;
-// const foo: T = 123;
-
-const obj = {
-    foo: 123,
-    bar: 'hello'
-};
-
-type T = typeof obj;
-const obj2: T = {
-    foo: 123,
-    bar: 'hello'
-};
-
-// 通常は型が最上位の事実となるため、type 変数名 = {変数名: 型} として型を定義する
-// 型は設計を表すものなので、実装に関しては型よりも下位にくることになる(設計に依存して書かれているため)
-// typeofを使用する場面は、値が最上位に来るケース。
-const commandList = ["attack", "defend", "run"] as const;
-type Command = typeof commandList[number];
