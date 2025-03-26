210683c739eb491972b8b29f9ac40552270c618e
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-03-27 08:41:21 +0900

    add: array notation

---
+++ practice/src/chap03/array.ts
@@ -0,0 +1,28 @@
+/**
+ * 配列の型
+ * 
+ */
+// const arr: number[] = [1, 10, 100];
+// 以下はコンパイルエラー
+// const arr2: number[] = [1, 10, "100"];
+
+// 配列の表現方法
+// 1. 配列の型を指定して宣言
+const arr: number[] = [1, 10, 100];
+
+// 2. T[]の代わりにジェネリック型を使用
+const arr2: Array<number> = [1, 10, 100];
+
+// 使い分け方の例
+// 1. 簡単な型の配列
+const arr3: string[] = ["foo", "bar", "baz"];
+
+// 2. 複雑な型の配列
+const arr4: Array<{ name: string; age: number }> = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+];
+
+/**
+ * readonly配列
+ */
