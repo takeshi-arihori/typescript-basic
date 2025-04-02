ccfde10af9dd002ce6f8b489c8234be5c4226fd1
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-04-02 21:54:48 +0900

    add: functions expression

--- practice/src/chap04/functions.ts
+++ practice/src/chap04/functions.ts
@@ -13,3 +13,45 @@ console.log(a);
 // const b = range(1, 10, 2);
 // const c = range("1", 10);
 
+
+/**
+ * セミコロン省略の罠
+ */
+function toSeconds(hours: number, minutes: number, seconds: number): number {
+    return hours * 3600 + minutes * 60 + seconds;
+
+    // 改行を行うと、return;と同じ意味になる
+    // return
+    //     hours * 3600
+    //         + minutes * 60
+    //         + seconds;
+}
+
+/**
+ * 関数式
+ */
+type Human = {
+    height: number,
+    weight: number,
+};
+
+// Human型はnumber型のheightとweightを持つオブジェクトの型である。
+// const calcBMI = function (human: Human): number {
+//     return human.weight / human.height ** 2;
+// };
+
+// 分割代入でも可能
+const calcBMI = function ({ height, weight }: Human): number {
+    return weight / height ** 2;
+};
+
+const uhyo: Human = {
+    height: 1.84,
+    weight: 72,
+};
+
+const bmi = calcBMI(uhyo);
+console.log(bmi);
+
+
+
