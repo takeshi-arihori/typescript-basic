1a4d5a056e61eee7569e4f3b4350b68c9b672d10
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-04-14 22:58:06 +0900

    add: omission type argument

--- practice/src/chap04/generics.ts
+++ practice/src/chap04/generics.ts
@@ -8,12 +8,72 @@ type User<N> = {
 };
 
 // ジェネリクスは関数が型引数を持ち、型引数に何が入るのかは関数を呼び出す際に決まる。
-function repeat<T>(element: T, length: number): T[] {
+// function repeat<T>(element: T, length: number): T[] {
+//     const result: T[] = [];
+//     for (let i = 0; i < length; i++) {
+//         result.push(element);
+//     }
+//     return result;
+// }
+// console.log(repeat("x", 5)); // [ 'x', 'x', 'x', 'x', 'x' ]
+// console.log(repeat(123, 3)); // [ 123, 123, 123 ]
+
+/**
+ * 関数の型引数を宣言する方法
+ */
+// function関数式の場合は関数の名前がない
+// const repeat = function<T>(element: T, length: number): T[] {
+//     const result: T[] = [];
+//     for (let i = 0; i < length; i++) {
+//         result.push(element);
+//     }
+//     return result;
+// }
+// アロー関数式の場合は引数リストの前にいきなり型引数リストを書く
+// const repeat = <T>(element: T, length: number): T[] => {
+//     const result: T[] = [];
+//     for (let i = 0; i < length; i++) {
+//         result.push(element);
+//     }
+//     return result;
+// }
+
+// メソッド記法
+const utils = {
+    repeat<T>(element: T, length: number): T[] {
+        const result: T[] = [];
+        for (let i = 0; i < length; i++) {
+            result.push(element);
+        }
+        return result;
+    }
+}
+
+// 型引数が複数のケース
+const pair = <Left, Right>(left: Left, right: Right): [Left, Right] => [left, right];
+const p = pair<string, number>("uhyo", 26);
+console.log(p); // [ 'uhyo', 26 ]
+
+// extends,オプショナル型引数
+const repeat = <T extends {
+    name: string;
+}>(element: T, length: number): T[] => {
     const result: T[] = [];
     for (let i = 0; i < length; i++) {
         result.push(element);
     }
     return result;
 }
-console.log(repeat("x", 5)); // [ 'x', 'x', 'x', 'x', 'x' ]
-console.log(repeat(123, 3)); // [ 123, 123, 123 ]
+
+type HasNameAndAge = {
+    name: string;
+    age: number;
+};
+
+console.log(repeat<HasNameAndAge>({
+    name: "uhyo",
+    age: 26,
+}, 3));
+
+// 制約を満たしていない場合はコンパイルエラーになる
+// console.log(repeat<string>("a", 4));
