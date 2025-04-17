92bb4bb124fe54bc2fc84410c8ece2058ee4ac52
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2025-04-17 22:17:19 +0900

    add: constructor

--- practice/src/chap05/basic.ts
+++ practice/src/chap05/basic.ts
@@ -1,6 +1,13 @@
 class User {
+    readonly id: number = 0; // Readonly property(コンストラクタでは初期化できる)
     name: string = "";
     age: number = 0;
+
+    constructor(id: number, name: string, age: number) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
     isAdult(): boolean {
         return this.age >= 20;
@@ -11,9 +18,9 @@ class User {
     }
 }
 
-const uhyo = new User();
-console.log(uhyo.isAdult()); // false
-
-uhyo.setAge(26);
-console.log(uhyo.age); // 26
+const uhyo = new User(1, "uhyo", 26);
 console.log(uhyo.isAdult()); // true
+
+uhyo.setAge(19);
+console.log(uhyo.age); // 19
+console.log(uhyo.isAdult()); // false
